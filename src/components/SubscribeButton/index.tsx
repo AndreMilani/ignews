77d96d4d674ac03302f9bc492aa1ae697055b279
AndreMilani <luisandremilani@gmail.com>
2022-10-25 22:30:17 -0300

    autenticar para ver posts
    commit 25-10-22

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,4 +1,7 @@
+import { Router } from 'express';
 import { useSession, signIn } from 'next-auth/react';
+import router, { useRouter } from 'next/router';
+import { useReducer } from 'react';
 import { api } from '../../services/api';
 import { getStripeJs } from '../../services/stripe-js';
 import styles from './styles.module.scss';
@@ -9,10 +12,17 @@ interface SubscribeButtonProps {
 
 export function SubscribeButton({ priceId}: SubscribeButtonProps) {
     const {data: session} = useSession();
+    const {} = useRouter()
 
+    
    async function handleSubscribe() {
         if (!session){
             signIn('github')
+            return;
+        }
+
+        if ( session.activeSubscription){
+            router.push('/posts')
             return;
         }
 
